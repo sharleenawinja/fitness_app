25f0b1b52f1b14a8de4fbde14ea60f686acdc83f
sharleenawinja <awinjasharleen@gmail.com>
2023-07-15 20:02:11 +0300

    creating home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 const Home = () => {
-  return <div>Home</div>;
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  console.log(bodyPart);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
 };
 
 export default Home;
